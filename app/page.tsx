59f11f08effeea92e01ad062bf16e9b8515988f8
ibelick <julien.thibeaut@gmail.com>
2023-05-10 09:27:08 +0200

    feat: remove section

--- app/page.tsx
+++ app/page.tsx
@@ -1,59 +1,9 @@
 import ComponentPlayground from '@/components/glazed/ComponentPlayground';
-import HeroSimpleCentered from '@/components/section/hero/HeroSimpleCentered';
-import HeroSplitWithScreenshot from '@/components/section/hero/HeroSplitWithScreenshot';
-import HeroSimpleCenteredWithBackgroundImage from '@/components/section/hero/HeroSimpleCenteredWithBackgroundImage';
-import FooterSimpleCentered from '@/components/section/footer/FooterSimpleCentered';
-import FooterSocialLinksOnly from '@/components/section/footer/FooterSocialLinksOnly';
-import NavbarBase from '@/components/section/navigation/NavbarBase';
-import Breadcrumb from '@/components/section/navigation/Breadcrumb';
-
-const COMPONENTS = [
-  {
-    name: 'Breadcrumb',
-    component: Breadcrumb,
-    isCentered: true,
-  },
-  {
-    name: 'NavbarBase',
-    component: NavbarBase,
-  },
-  {
-    name: 'HeroSimpleCentered',
-    component: HeroSimpleCentered,
-  },
-  {
-    name: 'HeroSplitWithScreenshot',
-    component: HeroSplitWithScreenshot,
-  },
-  {
-    name: 'HeroSimpleCenteredWithBackgroundImage',
-    component: HeroSimpleCenteredWithBackgroundImage,
-  },
-  {
-    name: 'FooterSimpleCentered',
-    component: FooterSimpleCentered,
-  },
-  {
-    name: 'FooterSocialLinksOnly',
-    component: FooterSocialLinksOnly,
-  },
-];
 
 export default function Home() {
   return (
     <main className='mx-auto min-h-screen max-w-7xl py-10'>
-      <div className='flex flex-col gap-8'>
-        {COMPONENTS.map((component) => {
-          return (
-            <div key={component.name}>
-              <h2 className='mb-2 text-xl font-medium'>{component.name}</h2>
-              <ComponentPlayground isCentered={component.isCentered}>
-                <component.component />
-              </ComponentPlayground>
-            </div>
-          );
-        })}
-      </div>
+      <div className='flex flex-col gap-8'></div>
     </main>
   );
 }

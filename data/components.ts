7de866ff1ca9fa3cbfceed1e8aa0419fdd12882f
ibelick <julien.thibeaut@gmail.com>
2023-05-31 18:54:29 +0200

    feat: rename file path

--- data/components.ts
+++ data/components.ts
@@ -1,21 +1,21 @@
-import TextAnimatedGradient from '@/components/lab/bla/TextAnimatedGradient';
-import TextGradient from '@/components/lab/bla/TextGradient';
-import TextShine from '@/components/lab/bla/TextShine';
-import ButtonBackgroundShine from '@/components/lab/bla3/ButtonBackgroundShine';
-import ButtonGradient from '@/components/lab/bla3/ButtonGradient';
-import ButtonHoverGradient from '@/components/lab/bla3/ButtonHoverGradient';
-import ButtonBackgroundSpotlight from '@/components/lab/bla3/ButtonBackgroundSpotlight';
-import ButtonRotatingBackgroundGradient from '@/components/lab/bla3/ButtonRotatingBackgroundGradient';
-import ButtonShadowGradient from '@/components/lab/bla3/ButtonShadowGradient';
-import InputGradientBorder from '@/components/lab/bla5/InputGradientBorder';
-import InputSpotlightBorder from '@/components/lab/bla5/InputSpotlightBorder';
-import InputPulseBorder from '@/components/lab/bla5/InputPulseBorder';
-import BadgeAnimatedGradientBorder from '@/components/lab/bla2/BadgeAnimatedGradientBorder';
-import BadgeShine from '@/components/lab/bla2/BadgeShine';
-import BadgeTextGradient from '@/components/lab/bla2/BadgeTextGradient';
-import CardAnimatedBorderGradient from '@/components/lab/bla4/CardAnimatedBorderGradient';
-import CardSpotlight from '@/components/lab/bla4/CardSpotlight';
-import CardPulseBorder from '@/components/lab/bla4/CardPulseBorder';
+import TextAnimatedGradient from '@/components/lab/text/TextAnimatedGradient';
+import TextGradient from '@/components/lab/text/TextGradient';
+import TextShine from '@/components/lab/text/TextShine';
+import ButtonBackgroundShine from '@/components/lab/button/ButtonBackgroundShine';
+import ButtonGradient from '@/components/lab/button/ButtonGradient';
+import ButtonHoverGradient from '@/components/lab/button/ButtonHoverGradient';
+import ButtonBackgroundSpotlight from '@/components/lab/button/ButtonBackgroundSpotlight';
+import ButtonRotatingBackgroundGradient from '@/components/lab/button/ButtonRotatingBackgroundGradient';
+import ButtonShadowGradient from '@/components/lab/button/ButtonShadowGradient';
+import InputGradientBorder from '@/components/lab/input/InputGradientBorder';
+import InputSpotlightBorder from '@/components/lab/input/InputSpotlightBorder';
+import InputPulseBorder from '@/components/lab/input/InputPulseBorder';
+import BadgeAnimatedGradientBorder from '@/components/lab/badge/BadgeAnimatedGradientBorder';
+import BadgeShine from '@/components/lab/badge/BadgeShine';
+import BadgeTextGradient from '@/components/lab/badge/BadgeTextGradient';
+import CardAnimatedBorderGradient from '@/components/lab/card/CardAnimatedBorderGradient';
+import CardSpotlight from '@/components/lab/card/CardSpotlight';
+import CardPulseBorder from '@/components/lab/card/CardPulseBorder';
 
 const TWCONFIG = {
   ['animated-background-shine']: {
@@ -40,109 +40,109 @@ export const GLAZED_COMPONENTS = [
     name: 'Text Gradient',
     component: TextGradient,
     slug: 'text-gradient',
-    type: 'bla',
+    type: 'text',
   },
   {
     name: 'Text Animated Gradient',
     component: TextAnimatedGradient,
     slug: 'text-animated-gradient',
-    type: 'bla',
+    type: 'text',
   },
   {
     name: 'Text Shine',
     component: TextShine,
     slug: 'text-shine',
-    type: 'bla',
+    type: 'text',
   },
   {
     name: 'Button Background Shine',
     component: ButtonBackgroundShine,
     slug: 'button-background-shine',
-    type: 'bla3',
+    type: 'button',
   },
   {
     name: 'Button Gradient',
     component: ButtonGradient,
     slug: 'button-gradient',
-    type: 'bla3',
+    type: 'button',
   },
   {
     name: 'Button Hover Gradient',
     component: ButtonHoverGradient,
     slug: 'button-hover-gradient',
-    type: 'bla3',
+    type: 'button',
   },
   {
     name: 'Button Background Spotlight',
     component: ButtonBackgroundSpotlight,
     slug: 'button-background-spotlight',
-    type: 'bla3',
+    type: 'button',
   },
   {
     name: 'Button Rotating Background Gradient',
     component: ButtonRotatingBackgroundGradient,
     slug: 'button-rotating-background-gradient',
-    type: 'bla3',
+    type: 'button',
   },
   {
     name: 'Button Shadow Gradient',
     component: ButtonShadowGradient,
     slug: 'button-shadow-gradient',
-    type: 'bla3',
+    type: 'button',
   },
   {
     name: 'Input Gradient Border',
     component: InputGradientBorder,
     slug: 'input-gradient-border',
-    type: 'bla5',
+    type: 'input',
   },
   {
     name: 'Input Spotlight Border',
     component: InputSpotlightBorder,
     slug: 'input-spotlight-border',
-    type: 'bla5',
+    type: 'input',
   },
   {
     name: 'Input Pulse Border',
     component: InputPulseBorder,
     slug: 'input-pulse-border',
-    type: 'bla5',
+    type: 'input',
   },
   {
     name: 'Badge Animated Gradient Border',
     component: BadgeAnimatedGradientBorder,
     slug: 'badge-animated-gradient-border',
-    type: 'bla2',
+    type: 'badge',
     twConfig: TWCONFIG['animated-background-shine'],
   },
   {
     name: 'Badge Shine',
     component: BadgeShine,
     slug: 'badge-shine',
-    type: 'bla2',
+    type: 'badge',
   },
   {
     name: 'Badge Text Gradient',
     component: BadgeTextGradient,
     slug: 'badge-text-gradient',
-    type: 'bla2',
+    type: 'badge',
   },
   {
     name: 'Card Animated Border Gradient',
     component: CardAnimatedBorderGradient,
     slug: 'card-animated-border-gradient',
-    type: 'bla4',
+    type: 'card',
   },
   {
     name: 'Card Spotlight',
     component: CardSpotlight,
     slug: 'card-spotlight',
-    type: 'bla4',
+    type: 'card',
   },
   {
     name: 'Card Pulse Border',
     component: CardPulseBorder,
     slug: 'card-pulse-border',
-    type: 'bla4',
+    type: 'card',
   },
 ];
